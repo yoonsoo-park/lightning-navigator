3cf1ffa2dc88f81fdd79358a17d0bf15793336bd
Yoonsoo Park <yoonsoo.park@ncino.com>
2024-10-27 12:50:37 -0400

    redo this too

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,66 @@
 const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: {
-    // Background script bundle
-    litnav_background_bundle: "./app/scripts/litnav_background.js",
-
-    // Content script bundle
-    content: [
-      "./app/scripts/jquery.js",
-      "./app/scripts/mousetrap.min.js",
-      "./app/scripts/forceTooling.js",
-      "./app/scripts/main.js",
-      "./app/scripts/sorttable.js",
+    entry: {
+        "scripts/service-worker": "./src/background/service-worker.js",
+        "scripts/content": "./src/content/index.js",
+        "scripts/offscreen": "./src/offscreen.js",
+    },
+    output: {
+        filename: "[name].js",
+        path: path.resolve(__dirname, "app"),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                        plugins: ["@babel/plugin-transform-runtime"],
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: "src/manifest.json",
+                    to: "manifest.json",
+                    transform(content) {
+                        return Buffer.from(
+                            JSON.stringify(
+                                {
+                                    ...JSON.parse(content.toString()),
+                                    version: process.env.npm_package_version,
+                                },
+                                null,
+                                2
+                            )
+                        );
+                    },
+                },
+                { from: "src/public/images", to: "images" },
+                { from: "src/public/_locales", to: "_locales" },
+                { from: "src/content/styles", to: "styles" },
+                { from: "src/offscreen.html", to: "offscreen.html" },
+            ],
+        }),
     ],
-  },
-  output: {
-    filename: "scripts/[name].js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  mode: "development", // Makes debugging easier
-  devtool: "source-map", // Helps with debugging
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
+    resolve: {
+        extensions: [".js"],
+        alias: {
+            "@": path.resolve(__dirname, "src"),
         },
-      },
-    ],
-  },
+    },
+    devtool:
+        process.env.NODE_ENV === "production"
+            ? "source-map"
+            : "cheap-module-source-map",
 };
